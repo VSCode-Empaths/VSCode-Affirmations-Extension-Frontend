b891d05de74a3ab69212ca949c45e4135a8faf3f
karl wernsman <wernsmank@gmail.com>
2022-12-13 10:10:23 -0800

    Can get error affirmation on command

--- extension.js
+++ extension.js
@@ -21,29 +21,18 @@ async function activate(context) {
   const dailyAffirmations = affirmations.filter((row) => row.category_id == 1);
   const randomNumber = Math.floor(Math.random() * dailyAffirmations.length);
   vscode.window.showInformationMessage(dailyAffirmations[randomNumber].text);
-  console.log(dailyAffirmations[randomNumber]);
 
-  // TODO get length of Affirmations
-  // TODO get random num within length of affirmations
-  // TODO get affirmation by index of random num
-
-  // const randomNumber = Math.floor(Math.random() * 10);
-  // const affirmations = await fetchAffirmations();
-  // vscode.window.showInformationMessage(affirmations[randomNumber].text);
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
+  // TODO FETCH ALL Affirmations by category.id (2 - Error)
   let disposable = await vscode.commands.registerCommand(
-    "helloworld.helloWorld",
+    "error_affirmations.getAffirmation(error)",
     async function () {
-      // The code you place here will be executed every time your command is executed
-      const affirmations = await fetchAffirmations();
-
-      const randomNumber = Math.floor(Math.random() * 10);
-
-      // Display a message box to the user
-      vscode.window.showInformationMessage(affirmations[randomNumber].text);
+      const errorAffirmations = affirmations.filter(
+        (row) => row.category_id == 2
+      );
+      const randomNumber = Math.floor(Math.random() * errorAffirmations.length);
+      vscode.window.showInformationMessage(
+        errorAffirmations[randomNumber].text
+      );
     }
   );
 
