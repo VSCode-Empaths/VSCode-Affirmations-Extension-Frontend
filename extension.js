d7b94ae9c4de70240644822e7f14425a6b2d8381
Rio Edwards <rioredwards@gmail.com>
2023-01-10 20:28:25 -0800

    Re-implement timer

--- extension.js
+++ extension.js
@@ -6,7 +6,7 @@ const { fetchAffirmations } = require("./fetch-utils.js");
  */
 async function activate(context) {
     // Constants
-    // const AFFIRMATION_INTERVAL = 1000 * 60 * 15; // ms * s * m
+    const AFFIRMATION_INTERVAL = 1000 * 60 * 15; // ms * s * m
     // const AFFIRMATION_INTERVAL = 1000; // ms * s * m
     let CAT_IDS = {
         DAILY: 1,
@@ -18,7 +18,7 @@ async function activate(context) {
     const affirmations = await fetchAffirmations();
 
     // Set timer and call affirmation on timerTick
-    // setInterval(displayRandAffirmationByCat, AFFIRMATION_INTERVAL, affirmations, CAT_IDS.DAILY);
+    setInterval(displayRandAffirmationByCat, AFFIRMATION_INTERVAL, affirmations, CAT_IDS.DAILY);
 
     // Display affirmation on page load
     displayRandAffirmationByCat(affirmations, CAT_IDS.DAILY);
